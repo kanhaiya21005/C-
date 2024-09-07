cec26053706c3788a8851319e89e3d11a8972854
Kanhaiya Gupta <kanhaiya212005@gmail.com>
2024-09-07 14:57:28 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number";
+    
+    while(true){
+        cin>>n;
+        if (n%10==0){
+            cout<<"loop breaks";
+            break;
+        }
+    }
+  
+    
+    
+
+}
